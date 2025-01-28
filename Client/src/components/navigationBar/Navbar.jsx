61df96ea22b1e73a7549dc21f9bf1b7b5e943ca6
bassplayarob <robwiberg89@gmail.com>
2025-01-27 20:50:26 -0700

    added html & styling to nav bar

--- Client/src/components/navigationBar/Navbar.jsx
+++ Client/src/components/navigationBar/Navbar.jsx
@@ -3,21 +3,20 @@ import './Navbar.css'
 
 export default function NavBar () {
   return (
-    <nav className='nav'>
-      <a href='/' className='site-title'>
-        Elevate Sports Garage
-      </a>
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-          <img
-            src='https://www.elevatesportsgarage.com/_next/image?url=%2Flogo.png&w=256&q=75'
-            alt='esg logo'
-          />
-          <h1>Elevate Sports Garage</h1>
-          <h2>Elevate Yourself</h2>
-        </li>
-      </ul>
+    <nav className='navbar'>
+      <div>
+        <img
+          className='logo'
+          src='https://www.elevatesportsgarage.com/_next/image?url=%2Flogo.png&w=256&q=75'
+          alt='esg logo'
+        />
+      </div>
+      <div className='nav-content'>
+        <h1 className='site-title'>Elevate Sports Garage</h1>
+        <p className='tagline'>
+          Elevate Your Game at Canyon Lake's Premier Batting Facility
+        </p>
+      </div>
     </nav>
   )
 }
