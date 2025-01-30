ac3b062c20b861a031a515791230773df2895edb
Tayyab004 <tayyabahmad2004@gmail.com>
2025-01-29 19:52:45 -0500

    services

--- Server/app.js
+++ Server/app.js
@@ -1,31 +1,27 @@
-import express from 'express';
-const app = express()
-const port = 3000
-import dotenv from 'dotenv';
+import express from "express";
+const app = express();
+const port = 3000;
+import dotenv from "dotenv";
 dotenv.config();
-import {connect} from './config/database.js'
-import AdminRoute from './routes/admin.route.js'
-import ServicesRoute from './routes/services.route.js'
-import ReviewsRoute from './routes/reviews.route.js'
-import cors from 'cors'
-
+import { connect } from "./config/database.js";
+import AdminRoute from "./routes/admin.route.js";
+import ServicesRoute from "./routes/services.route.js";
+import ReviewsRoute from "./routes/reviews.route.js";
+import cors from "cors";
 
 connect();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use('/', AdminRoute)
-app.use('/', ServicesRoute)
-app.use('/',ReviewsRoute)
+app.use("/", AdminRoute);
+app.use("/", ServicesRoute);
+app.use("/", ReviewsRoute);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
