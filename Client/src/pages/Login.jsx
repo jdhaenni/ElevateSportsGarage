46c97ece9898a1060d75a83bbabded189588a44d
jdhaenni <jdhaenni@gmail.com>
2025-01-30 22:58:56 -0500

    login gets token back

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,24 +1,50 @@
 import React from 'react'
-
-import { useEffect,useState } from 'react'
+import { loginAdmin } from '../api/LoginApi';
+import {useState } from 'react'
 
 
 export default function Admin() {
   const [formData, setFormData] = useState({
-    username: "",
+    user: "",
     password: "",
 });
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  console.log(formData)
+  try {
+
+      const token = await loginAdmin(formData);
+      
+
+      localStorage.setItem("token", token);
+
+      
+      console.log(token)
+      // navigate("/protected");
+  } catch (error) {
+      console.log(error);
+      
+  }
+};
+
  
+const handleChange = (e) => {
+  setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+  }));
+};
 
 
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>User Name</label><br></br>
-        <input type='text' name = 'user' value = {formData.user}></input><br></br>
+        <input type='text' name = 'user' value = {formData.user} onChange={handleChange}></input><br></br>
         <label>Password</label><br></br>
-        <input type='password' name = 'password' value = {formData.password}></input><br></br>
+        <input type='password' name = 'password' value = {formData.password} onChange={handleChange}></input><br></br>
         <button type='submit'>Login</button>
       </form>
       
