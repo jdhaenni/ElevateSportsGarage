13a5a28f40365dc8014307071b1ae6ccef2369df
jdhaenni <jdhaenni@gmail.com>
2025-02-02 10:37:55 -0500

    added api functions for reviews and started to workin them into admin panel

---
+++ Client/src/api/ReviewsApi.js
@@ -0,0 +1,36 @@
+import AuthService from "./AuthService";
+
+// fetch all reviews
+export const fetchAllReviews = async () => {
+  try {
+     const response = await AuthService.get("/reviews");
+    response => response.json
+  return response.data;
+  }
+  catch (error) {
+    
+  }
+};
+
+// fetch a single review by id
+export const fetchReview = async (id) => {
+  const response = await AuthService.get(`/reviews/${id}`);
+  return response.data;
+};
+
+// create a new review (available only to admin)
+export const createReview = async (reviewData) => {
+  const response = await AuthService.post("/reviews", reviewData);
+  return response.data;
+};
+
+// update an existing service (available only to admin)
+export const updateReview = async (id, reviewData) => {
+  const response = await AuthService.put(`/reviews/${id}`, reviewData);
+  return response.data;
+};
+
+// delete an existing service (available only to admin)
+export const deleteReview = async (id) => {
+  await AuthService.delete(`/reviews/${id}`);
+};
--- Client/src/pages/Admin.jsx
+++ Client/src/pages/Admin.jsx
@@ -1,35 +1,76 @@
 import React from 'react'
-import { fetchAllServices } from '../api/ServicesApi'
-import { useState,useEffect } from 'react'
+import {
+  fetchAllServices,
+  fetchService,
+  createService,
+  updateService,
+  deleteService
+} from '../api/ServicesApi'
+import {
+  fetchAllReviews,
+  fetchReview,
+  createReview,
+  updateReview,
+  deleteReview
+} from '../api/ReviewsApi'
+import { useState, useEffect } from 'react'
 
-export default function Admin() {
-    const [services,setServices] = useState([])
+export default function Admin () {
+  const [services, setServices] = useState([])
 
-   useEffect(() => {
-    const fetchServices = async ()=>{
+  useEffect(() => {
+    const fetchServices = async () => {
       const data = await fetchAllServices()
       setServices(data)
     }
     fetchServices()
-  },[])
+  }, [])
 
   console.log(services)
-    
+
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      const data = await fetchAllReviews()
+      setReviews(data)
+    }
+    fetchReviews()
+  }, [])
+
+  console.log(reviews)
+
 
   return (
     <div>
-        <ul>
-        {services.map((service)=>{
-        return(
-        <li key = {service._id}>
-          <p>
-            {service.name}<br></br>
-            {service.price}
-          </p>
-        </li>)
-      })}
+      <ul>
+        {services.map(service => {
+          return (
+            <li key={service._id}>
+              <p>
+                {service.name}
+                <br></br>
+                {service.price}
+              </p>
+            </li>
+          )
+        })}
       </ul>
-      
+      <ul>
+        {reviews.map(review => {
+          return (
+            <li key={review._id}>
+              <p>
+                {review.name}
+                <br></br>
+                {review.stars}
+              </p>
+            </li>
+          )
+        })}
+      </ul>
     </div>
+    
+    
   )
 }
