f1f6dc92bd01d799db0aa681f42d7a22816c0ac1
jdhaenni <jdhaenni@gmail.com>
2025-02-03 11:19:16 -0500

    CREATE review and service added to ADMIN

--- Client/src/pages/Admin.jsx
+++ Client/src/pages/Admin.jsx
@@ -14,6 +14,7 @@ import {
   deleteReview
 } from '../api/ReviewsApi'
 import { useState, useEffect } from 'react'
+import AuthService from '../api/AuthService'
 
 export default function Admin () {
   const [services, setServices] = useState([])
@@ -26,7 +27,46 @@ export default function Admin () {
     fetchServices()
   }, [services])
 
- 
+  const [serviceFormData, setServiceFormData] = useState({
+    name: '',
+    description: '',
+    price: ''
+  })
+  const [reviewFormData, setReviewFormData] = useState({
+    name: '',
+    date: '',
+    stars: '',
+    body:''
+  })
+  const handleServiceSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      createService(serviceFormData)
+      setServiceFormData({
+        name: '',
+        description: '',
+        price: ''
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const handleReviewSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      createReview(reviewFormData)
+      setReviewFormData({
+        name: '',
+        date: '',
+        stars: '',
+        body:""
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const [reviews, setReviews] = useState([])
 
@@ -38,19 +78,44 @@ export default function Admin () {
     fetchReviews()
   }, [reviews])
 
-  const deleteServiceButton = async (e)=>{
+  const deleteServiceButton = async e => {
     deleteService(e.target.name)
- }
+  }
 
- const deleteReviewButton = async (e)=>{
+  const deleteReviewButton = async e => {
     deleteReview(e.target.name)
- }
+  }
+   
+const handleServiceChange = (e) => {
+  setServiceFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+  }));
+};
+const handleReviewChange = (e) => {
+  setReviewFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+  }));
+};
 
 
   return (
     <div>
       SERVICES!<br></br>
-      <button >CREATE</button><br></br>
+      NEW SERVICE<br></br>
+      <form></form> <form onSubmit={handleServiceSubmit}>
+        <label>Service Name</label><br></br>
+        <input type='text' name = 'name' value = {serviceFormData.name} onChange={handleServiceChange}></input><br></br>
+        <label>Description</label><br></br>
+        <input type='text' name = 'description' value = {serviceFormData.description} onChange={handleServiceChange}></input><br></br>
+        <label>Price</label><br></br>
+        <input type='text' name = 'price' value = {serviceFormData.price} onChange={handleServiceChange}></input><br></br>
+        <button type='submit'>Create New Service</button>
+      </form>
+      
+      
+      <br></br>
       <ul>
         {services.map(service => {
           return (
@@ -58,36 +123,60 @@ export default function Admin () {
               <p>
                 {service.name}
                 <br></br>
-                {service.description}<br></br>
-                {service.price}<br></br>
-                <button >UPDATE</button><br></br>
-                <button name={service._id} onClick={deleteServiceButton} >DELETE</button><br></br>
+                {service.description}
+                <br></br>
+                {service.price}
+                <br></br>
+                <button>UPDATE</button>
+                <br></br>
+                <button name={service._id} onClick={deleteServiceButton}>
+                  DELETE
+                </button>
+                <br></br>
               </p>
             </li>
           )
         })}
       </ul>
       <ul>
-      REVIEWS!<br></br>
-      <button >CREATE</button><br></br>
+        REVIEWS!<br></br>
+        NEW REVIEW<br></br>
+      <form></form> <form onSubmit={handleReviewSubmit}>
+        <label>Name</label><br></br>
+        <input type='text' name = 'name' value = {reviewFormData.name} onChange={handleReviewChange}></input><br></br>
+        <label>Date</label><br></br>
+        <input type='text' name = 'date' value = {reviewFormData.date} onChange={handleReviewChange}></input><br></br>
+        <label>Stars</label><br></br>
+        <input type='text' name = 'stars' value = {reviewFormData.stars} onChange={handleReviewChange}></input><br></br>
+        <label>Body</label><br></br>
+        <input type='text' name = 'body' value = {reviewFormData.body} onChange={handleReviewChange}></input><br></br>
+        <button type='submit'>Create New Review</button>
+      </form>
+        
+        <br></br>
         {reviews.map(review => {
           return (
             <li key={review._id}>
               <p>
                 {review.name}
                 <br></br>
-                {review.stars}<br></br>
-                {review.date}<br></br>
-                {review.body}<br></br>
-                <button  >UPDATE</button><br></br>
-                <button name={review._id} onClick={deleteReviewButton}>DELETE</button><br></br>
+                {review.stars}
+                <br></br>
+                {review.date}
+                <br></br>
+                {review.body}
+                <br></br>
+                <button>UPDATE</button>
+                <br></br>
+                <button name={review._id} onClick={deleteReviewButton}>
+                  DELETE
+                </button>
+                <br></br>
               </p>
             </li>
           )
         })}
       </ul>
     </div>
-    
-    
   )
 }
--- notes.md
+++ notes.md
@@ -1,1 +1,2 @@
 ask chris about login !token/protected routes token validation
+services/reviews not defined on page load error
