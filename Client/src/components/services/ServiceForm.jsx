9730db8ebf1d6f7d664bc733dd80687fa1e21998
jdhaenni <jdhaenni@gmail.com>
2025-02-05 18:15:21 -0500

    service merge conflict

--- Client/src/components/services/ServiceForm.jsx
+++ Client/src/components/services/ServiceForm.jsx
@@ -2,15 +2,8 @@ import React, { useState } from 'react'
 
 const ServiceForm = ({ onCreate }) => {
   const [formData, setFormData] = useState({
-<<<<<<< HEAD
     name: '',
     description: '',
     price: ''
   })
 }
-=======
-    name: "",
-    description: "",
-    price: "",
-  });
->>>>>>> 52968f00488fac71f779530085d4ac4efcb1bd40
--- Client/src/components/services/ServiceItem.jsx
+++ Client/src/components/services/ServiceItem.jsx
@@ -5,11 +5,6 @@ const ServiceItem = ({ service, onUpdate, onDelete }) => {
   const [editForm, setEditForm] = useState({
     name: service.name,
     description: service.description,
-<<<<<<< HEAD
     price: service.price
   })
 }
-=======
-    price: service.price,
-  });
->>>>>>> 52968f00488fac71f779530085d4ac4efcb1bd40
