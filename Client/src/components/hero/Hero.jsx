eb5e546e676397e2488c9bedbb139b47635e62db
bassplayarob <robwiberg89@gmail.com>
2025-02-05 16:45:51 -0700

    hero section css

--- Client/src/components/hero/Hero.jsx
+++ Client/src/components/hero/Hero.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react'
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero-content">
+    <div className='hero'>
+      <div className='hero-content'>
         <h1>Elevate Sports Garage</h1>
         <p>Your one-stop shop for all your sports needs</p>
-        <button className="hero-button">Book Now</button>
+        <button className='hero-button'>Book Now</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
