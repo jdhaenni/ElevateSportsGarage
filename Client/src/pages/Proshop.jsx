c1aab171ce67ef12a3a2821369a5622876d18ca6
bassplayarob <robwiberg89@gmail.com>
2025-02-11 21:47:22 -0700

    added some more styling to proshop page

--- Client/src/pages/Proshop.jsx
+++ Client/src/pages/Proshop.jsx
@@ -5,17 +5,46 @@ const products = [
   {
     id: 1,
     name: 'Baseball bats',
-    price: 80
+    price: 80,
+    image: (
+      <img
+        src='https://www.elevatesportsgarage.com/_next/image?url=%2Fbaseball-bat.png&w=256&q=75'
+        alt='baseball bat'
+      />
+    )
   },
   {
     id: 2,
     name: 'Batting Gloves',
-    price: 50
+    price: 50,
+    image: (
+      <img
+        src='https://www.elevatesportsgarage.com/_next/image?url=%2Fbatting-gloves.png&w=256&q=75'
+        alt='batting gloves'
+      />
+    )
   },
   {
     id: 3,
     name: 'ESG t-shirt',
-    price: 40
+    price: 40,
+    image: (
+      <img
+        src='https://www.elevatesportsgarage.com/_next/image?url=%2Fesg-t-shirt.png&w=256&q=75'
+        alt='esg t-shirt'
+      />
+    )
+  },
+  {
+    id: 4,
+    name: 'Baseball/Softball 3-pack',
+    price: 25,
+    image: (
+      <img
+        src='https://www.elevatesportsgarage.com/_next/image?url=%2Fbaseball-softball-3-pack.png&w=256&q=75'
+        alt='baseball/softball 3-pack'
+      />
+    )
   }
 ]
 
@@ -34,6 +63,9 @@ export default function Proshop () {
             <h2>{product.image}</h2>
             <h3>{product.name}</h3>
             <p>${product.price}</p>
+            <button onClick={() => addToCart(product)} className='add-to-cart'>
+              Add to Cart
+            </button>
           </div>
         ))}
       </div>
