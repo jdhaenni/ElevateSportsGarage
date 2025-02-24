14c44dbb66c9d39750c701e8c841d4de6a88914e
jdhaenni <jdhaenni@gmail.com>
2025-02-24 12:44:12 -0500

    admin sidebar works, just need to put update back in

--- Client/src/pages/Admin.jsx
+++ Client/src/pages/Admin.jsx
@@ -1,43 +1,36 @@
-import React from 'react'
-import AdminServices from '../components/admin/AdminServices'
-import AdminReviews from '../components/admin/AdminReviews'
-import './Admin.css'
-import AdminHours from '../components/admin/AdminHours'
-import AdminContacts from '../components/admin/AdminContacts'
+import React, { useState } from 'react';
+import AdminServices from '../components/admin/AdminServices';
+import AdminReviews from '../components/admin/AdminReviews';
+import AdminContacts from '../components/admin/AdminContacts';
+import AdminHours from '../components/admin/AdminHours';
+import './Admin.css';
 
+export default function Admin() {
+  // State to track which section is selected
+  const [selectedSection, setSelectedSection] = useState('services');
 
-
-
-export default function Admin () {
+  // Function to handle click event on sidebar items
+  const handleSidebarClick = (section) => {
+    setSelectedSection(section);
+  };
 
   return (
-    
     <div className='container'>
-      <div className='side-bar'><ul>
-          <li >
-            Services
-          </li>
-          <li >
-           Reviews
-          </li>
-          <li >
-            Contacts
-          </li>
-          <li>
-            Products
-          </li>
-          <li>
-            Hours
-          </li>
-        </ul></div>
-        <div className='display-container'>
-      <AdminServices className="services"/>
-      <AdminReviews className="reviews" />
-      <AdminContacts className="contacts" />
-      <AdminHours  />
+      <div className='side-bar'>
+        <ul>
+          <li onClick={() => handleSidebarClick('services')}>Services</li>
+          <li onClick={() => handleSidebarClick('reviews')}>Reviews</li>
+          <li onClick={() => handleSidebarClick('contacts')}>Contacts</li>
+          <li onClick={() => handleSidebarClick('hours')}>Hours</li>
+        </ul>
       </div>
-      
-      
+
+      <div className='display-container'>
+        {selectedSection === 'services' && <AdminServices className="services" />}
+        {selectedSection === 'reviews' && <AdminReviews className="reviews" />}
+        {selectedSection === 'contacts' && <AdminContacts className="contacts" />}
+        {selectedSection === 'hours' && <AdminHours className="hours" />}
+      </div>
     </div>
-  )
+  );
 }
